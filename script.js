f43f9b7f9bf80d7a99fd92b2891724d054725255
Elmaea <elma.anish06@gmail.com>
2025-01-25 18:09:01 +0530

    added a new js file

---
+++ script.js
@@ -0,0 +1,8 @@
+// Navigation functionality
+function navigate(page) {
+    alert(`Navigating to ${page} page!`);
+    // Here you can implement actual navigation logic
+    // For example:
+    // window.location.href = `${page}.html`;
+  }
+  
